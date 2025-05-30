2b54099fc0b2f4411b12000e69a74a0e5bfd332b
Jayfx24 <176495720+Jayfx24@users.noreply.github.com>
2025-05-30 03:34:09 -0400

    Added eslint & prettier

--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  
+
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
@@ -28,7 +28,6 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
-     
     ],
   },
 };
